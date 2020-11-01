bdd63b5c3cc2f6210405c4dbacd5da77a9af3838
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2020-11-01 11:12:32 +0530

    Code11.py

--- code11.py
+++ code11.py
@@ -1,20 +1,17 @@
 
-#A = list(map(int,input("").split(" ")))
+A = list(map(int,input("").split(" ")))
 
-A=[1,1,1,1,4,5,3,2,1,6,7]
 A.sort()
-#print(A)
 new=0
 x=0
 
 for i in A:
     if i==x:
         new+=1
-        #print(i,":\t",new)
-
-    else:
         if new>=(len(A)//2):
             print(x)
+        
+    else:
+        
         new=1
-        #print(i,":\t",new)
         x=i
