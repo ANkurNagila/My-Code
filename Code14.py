6b55231e51f2510cd8e333d8c2d8c36d80014120
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2020-11-04 23:14:17 +0530

    Update

---
+++ Code14.py
@@ -0,0 +1,17 @@
+N,Q=map(int,input().split(" "))
+
+A=list(map(int,input().split(" ")))
+
+while Q>0:
+    L,R=map(int,input().split(" "))
+    sum=0
+    for i in range(L-1,int(R/2)+1):
+        sum=sum+A[i]+A[R-i]
+        #print(sum)
+
+    if (R-L)%2==0:
+        sum=sum-A[int((L+R)/2)-1]
+    mean=sum/(R-L+1)
+
+    print(int(mean))
+    Q-=1
