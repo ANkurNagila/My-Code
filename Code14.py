d89c6ab69c07e41557ddd6b57e529fa6bf3bbadd
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2020-11-08 12:08:10 +0530

    Play with numbers

--- Code14.py
+++ Code14.py
@@ -1,18 +1,31 @@
+import statistics
+
 N,Q=map(int,input().split(" "))
 
-A=list(map(int,input().split(" ")))
-
-while Q>0:
-    
+Whole=list(map(int,input().split(" ")))
+while Q>0:                                                                    ##Time Limit error
     L,R=map(int,input().split(" "))
-    sum=0
-    for i in range(L-1,int(R/2)+1):
-        sum=sum+A[i]+A[R-i]
-        #print(sum)
 
-    if (R-L)%2==0:
-        sum=sum-A[int((L+R)/2)-1]
-    mean=sum/(R-L+1)
-
-    print(int(mean))
+    print(int(statistics.mean(Whole[L-1:R])))
     Q-=1
+
+
+    
+    
+    
+        ################################################################################################################################# 
+m, n = map(int, input().split())
+a = list(map(int, input().split()))
+s = []
+s1 = 0
+for i in range(m):
+    s1 += a[i]
+    s.append(s1)                                                                ##Correct Method
+for i in range(n):
+    x, y = map(int, input().split())
+    if x == 1:
+        print((s[y-1])//(y-x+1))
+    else:
+        print((s[y-1] - s[x-2])//(y-x+1))
+        
+        #######################################################################################################################################
