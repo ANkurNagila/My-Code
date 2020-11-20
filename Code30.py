47fca6a8cd469d1b37b1555de062d1f9baa66049
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2020-11-20 23:26:08 +0530

    Update

---
+++ Code30.py
@@ -0,0 +1,47 @@
+def multiples(N):
+    t=[]
+    c=N
+    for x in range(2,N+1):
+        if c%x==0:
+            t.append(x)
+            c=c/x
+    #print("N:",N,"T:",t)
+    return t
+
+def removing_some_multiples(t):
+    for x in t:
+        j=multiples(x)
+        for i in j:
+            if i in t and i!=x:
+                t.remove(i)
+
+    return t
+
+
+
+def lcm(D):
+    #print(D)
+    f={}
+    result=1
+    for x in D:
+        x=int(x)
+        #print(x)
+        f[x]=multiples(x)
+    common=list()
+    for i in f.values():
+        for j in i:
+            if j not in common:
+                common.append(j)
+
+    #print(common)
+
+    removing_some_multiples(common)
+
+    #print(common)
+    for k in common:
+        result=result*k
+    return result
+
+
+D = input().split(" ")
+print(lcm(D))
