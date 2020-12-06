7dd097c68bc53ec4d3ca490820d589a0a22a97a7
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2020-12-06 23:33:19 +0530

    Case sensitive

---
+++ Code46.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    s = input()
+    s1 = s[0].lower()
+    for c in s[1:]:
+        if ord(c) < 97:
+            s1 += '_'+c.lower()
+            continue
+        s1 += c
+    print(s1)
